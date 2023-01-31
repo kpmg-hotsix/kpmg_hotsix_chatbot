0341cc84cba262b7e5c98694f572b5f67349bf01
momozzing <ahdbsgh2@naver.com>
2023-01-31 14:58:21 +0900

    add korfin finetuning

---
+++ train.sh
@@ -0,0 +1,5 @@
+deepspeed --num_gpus=2 korfin_train.py --model_name lighthouse/mdeberta-v3-base-kor-further
+deepspeed --num_gpus=2 korfin_train.py --model_name jinmang2/kpfbert
+deepspeed --num_gpus=2 korfin_train.py --model_name klue/roberta-large
+deepspeed --num_gpus=2 korfin_train.py --model_name tunib/electra-ko-base
+deepspeed --num_gpus=2 korfin_train.py --model_name lassl/bert-ko-base
