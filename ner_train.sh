c05ac26f00d7cc769fefd11078d2155b7745d29a
momozzing <ahdbsgh2@naver.com>
2023-02-03 23:41:21 +0900

    add task_train.sh

---
+++ ner_train.sh
@@ -0,0 +1,10 @@
+python NER_train.py -c config.json --model_name lighthouse/mdeberta-v3-base-kor-further
+python NER_train.py -c config.json --model_name jinmang2/kpfbert
+python NER_train.py -c config.json --model_name klue/roberta-large
+python NER_train.py -c config.json --model_name tunib/electra-ko-base
+python NER_train.py -c config.json --model_name lassl/bert-ko-base
+python NER_train.py -c config.json --model_name snunlp/KR-FinBert-SC
+python NER_train.py -c config.json --model_name KB/bert-base-swedish-cased
+python NER_train.py -c config.json --model_name beomi/kcbert-base
+python NER_train.py -c config.json --model_name beomi/KcELECTRA-base-v2022
+python NER_train.py -c config.json --model_name snunlp/KR-Medium
