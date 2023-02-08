d0138ce11817cf24ba95d06a6b8b71544d433f4e
momozzing <ahdbsgh2@naver.com>
2023-02-09 01:59:03 +0900

    add interface code

--- interface.py
+++ interface.py
@@ -1,8 +1,31 @@
-
-
+import re
+from data.news_search import news_search
 
 while True:
     t = input("\nchat: ")
-    text = 콴다 기업과 비슷한 
-    
-    t = 
+    '''
+    task1_text = "자연어 처리 기술과 관련된 기업 알려줘"
+    task2_text = "콴다 기업과 유사한 기업 찾아줘"
+    task3_text = "콴다 기업의 최근 이슈 알려줘"
+    task4_text = "콴다 기업의 재무재표 알려줘"
+    '''
+
+    if "기술과" in t:
+        print(t.split("기술과")[0])
+        print('task1') 
+
+    if "유사한 기업" in t:
+        company = t.split("유사한 기업")[0].replace('과', '')
+        print(company) 
+        print('task2') 
+
+    if "최근 이슈" in t:
+        company = t.split("최근 이슈")[0].replace('의', '')
+        print(company) 
+        print('task3') 
+        print(news_search(company))
+        
+    if "재무재표" in t:
+        company = t.split("재무재표")[0].replace('의', '')
+        print(company) 
+        print('task4') 
