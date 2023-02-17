f3d73bcada9050be6668d472c70175ea2cd58851
Jungahn Yang <jungahn00@gmail.com>
2023-02-18 01:54:37 +0900

    add tech_search

--- Tech_search.py
+++ Tech_search.py
@@ -1,13 +1,23 @@
 import pandas as pd
 
-data = pd.read_csv("data/keyword_DB.csv", sep = ',')
-tmp = []
+keywordDB = pd.read_csv("data/keyword_DB.csv")
+# name 은 list
+# interface 쪽에서 3개 이상 차단.
 
-def Tech_search(name):
-    for idx, key in enumerate(data['keyword']):
-        if name in key:
-            tmp.append(data['company'][idx])
-    return tmp
+def Tech_search(tech):
+    keywords = []
+
+    for i in reversed(range(len(tech)+1)):
+        # print(i)
+        for idx, keys in enumerate(keywordDB['keyword']):
+            keys = eval(keys)
+            matching = set(tech) & set(keys)
+            if len(matching) == i:
+                # print(i, tech, keys)
+                keywords.append(keywordDB['company'][idx])
+                if len(keywords) == 5:
+                    break
+    return keywords
     
 
-print(Tech_search('자율주행')[:5])
+# print(Tech_search('자율주행')[:5])
